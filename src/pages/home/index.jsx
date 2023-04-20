f57e90538108b3fda74bfac98817a32c160c2618
Dinesh <iamdineshbasnet@gmail.com>
2023-04-20 08:47:59 +0545

    top rated page added

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import HeroBanner from './HeroBanner'
 import Trending from './Trending'
 import Popular from './Popular'
+import TopRated from './TopRated'
 const index = () => {
   return (
     <div className='homePage'>
         <HeroBanner />
         <Trending />
         <Popular />
+        <TopRated />
     </div>
   )
 }
