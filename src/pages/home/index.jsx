7bf2b18123fbe55891d4547da7f29d3611cfc253
Dinesh <iamdineshbasnet@gmail.com>
2023-04-20 08:37:36 +0545

    popular section added in homepage

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HeroBanner from './HeroBanner'
 import Trending from './Trending'
+import Popular from './Popular'
 const index = () => {
   return (
     <div className='homePage'>
         <HeroBanner />
         <Trending />
+        <Popular />
     </div>
   )
 }
